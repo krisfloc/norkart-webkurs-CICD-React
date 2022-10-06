ac56285f189b398e5bf8f08bc2cd3772e1d0576c
krisfloc <kristian9818@hotmail.com>
2022-10-06 17:58:54 +0200

    hammer

--- src/components/LocationMenu.js
+++ src/components/LocationMenu.js
@@ -13,7 +13,7 @@ const locations = [
   { center: [10.674706290112326, 59.92544400487645], name: "Skøyen" },
   { center: [10.408773, 63.422091], name: "Trondheim" },
   { center: [7.993744574800331, 58.145072155868036], name: "Kristiansand" },
-  { center: [10.457060447232827, 61.123876982347184], name: "LilleHammer" },
+  { center: [10.457060447232827, 61.123876982347184], name: "Lillehammer" },
   { center: [5.341447457437576, 60.372653021429045], name: "Bergen" },
 ];
 
